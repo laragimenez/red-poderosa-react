662102bf70ad640772350485b3de06b19c14f8ce
janetitaliamamani <janet.mamani@itscordoba.edu.ar>
2025-03-13 19:52:13 -0300

    se aplico el fondo para reflejar el numero de conectados a la red social

--- red-poderosa/src/componentes/Home/MyHome.jsx
+++ red-poderosa/src/componentes/Home/MyHome.jsx
@@ -1,14 +1,52 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import React from 'react'
+import './MyHome.css';
+import React, { useState, useEffect } from 'react';
 
 const MyHome = () => {
+  const [connectedUsers, setConnectedUsers] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  // Función para obtener los usuarios conectados
+  const fetchConnectedUser = async () => {
+    try {
+      setLoading(true); // Empezar a cargar
+      let response = await fetch('http://localhost:5297/LoginCount');  
+      let data = await response.json();
+      console.log("Datos de la API:", data);
+      setConnectedUsers(data.numberLogin); // "numberLogin" es la propiedad que contiene el número de usuarios conectados.
+    } catch (e) {
+      console.error('Error fetching users:', e);
+    } finally {
+      setLoading(false); // Finaliza la carga
+    }
+  };
+
+  // Llamar a la función cuando el componente se monte
+  useEffect(() => {
+    fetchConnectedUser();
+  }, []); // El array vacío [] asegura que la función solo se ejecute una vez al montar el componente
+
   return (
-    <div>
-      <h2>Bienvenido al Dashboard</h2>
+    <div className="dashboard-container">
+      <h2>RED PODEROSA</h2>
       <h1>Bienvenido al Panel de Administración</h1>
+
+      <div className="stat-card">
+        <h2>Usuarios Conectados</h2>
+
+        {/* Si loading es true, mostramos "Cargando..." */}
+        {loading ? (
+          <p className="loading-text">Cargando...</p>
+        ) : (
+          <div className="connected-users-container">
+            <p className="connected-users">
+              <span className="big-number">{connectedUsers}</span>
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 
 export default MyHome;
+
